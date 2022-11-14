983c9212314fa780557e85d32c0c7b228c8e9fd9
sm8sk4l <sebastianmoskal@protonmail.com>
2022-11-14 23:13:10 +0100

    better solution with reduce method

--- Running_Sum_of_1d_Array/solution.js
+++ Running_Sum_of_1d_Array/solution.js
@@ -1,9 +1,4 @@
-var runningSum = function (nums) {
-  const arr = [];
-  let counter = 0;
-  for (let index = 0; index < nums.length; index++) {
-    counter += nums[index];
-    arr.push(counter);
-  }
-  return arr;
+const runningSum = function (nums) {
+  nums.reduce((acc, num, index, arr) => (arr[index] += acc));
+  return nums;
 };
