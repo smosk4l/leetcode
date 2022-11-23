bac4bda301b1e40b14b174919f6acc9dd96162e5
sm8sk4l <sebastianmoskal@protonmail.com>
2022-11-23 18:56:55 +0100

    longest-common-prefix done

--- longest-common-prefix/solution.js
+++ longest-common-prefix/solution.js
@@ -1,30 +1,19 @@
 const longestCommonPrefix = function (strs) {
+  const size = strs.length;
+  if (size === 0) return "";
+  if (size === 1) return strs[0];
+
   let prefix = "";
-  for (let i = 0; i < strs.length; i++) {
-    const str = strs[i];
-    for (let j = 0; j < str.length; j++) {
-      const letter = str[j];
-      console.log(letter);
-    }
+  let counter = 0;
+  let isSearching = true;
+
+  while (isSearching) {
+    if (strs.every((str) => str.startsWith(prefix + strs[0][counter]))) {
+      prefix += strs[0][counter];
+      counter++;
+    } else isSearching = false;
   }
-  // if (!strs.every((str) => str.includes(findFirstPrefix(1))) || strs[0] === "")
-  //   return "";
-
-  // if (strs.length === 1 && strs[0].length === 1) return strs[0];
-
-  // let counter = 1;
-  // let isSearching = true;
-  // while (isSearching) {
-  //   const findAllPrefix = strs.every((str) =>
-  //     str.includes(findFirstPrefix(counter))
-  //   );
-  //   if (findAllPrefix) counter++;
-  //   else {
-  //     counter--;
-  //     isSearching = false;
-  //   }
-  // }
-  // return findFirstPrefix(counter);
+  return prefix;
 };
 
-longestCommonPrefix(["flower", "flow", "flight"]);
+console.log(longestCommonPrefix(["flower", "flow", "floight"]));
