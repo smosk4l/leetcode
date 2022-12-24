652235f5c0da2cafb9c7149791a361a50cefae64
sm8sk4l <sebastianmoskal@protonmail.com>
2022-12-24 01:39:48 +0100

    better solution

--- Palindrome_Number/solution.js
+++ Palindrome_Number/solution.js
@@ -1,10 +1,5 @@
 const isPalindrome = function (x) {
-  if (x < 0) return false;
-  const str = String(x);
-  let palindromeStr = "";
-  for (let i = str.length - 1; i >= 0; i--) {
-    palindromeStr += str[i];
-  }
-  if (Number(palindromeStr) === x) return true;
-  return false;
+  return x === Number(x.toString().split("").reverse().join(""));
 };
+
+console.log(isPalindrome(121));
